cf94dddb989345033234867addf05d385337efc7
Daniel Calderon <danielcalderon1199@gmail.com>
2023-03-23 21:26:08 -0700

    Adding phase 4

--- all-dragons.js
+++ all-dragons.js
@@ -1,5 +1,19 @@
-// Your code here
+const FriendlyDragon = require('./classes/friendly-dragon');
+const EvilDragon = require('./classes/evil-dragon');
+const Dragons = require('./classes/dragon');
 
+const falkor =  new FriendlyDragon('Falkor',
+'white',
+["save Atreyu from the swamp", "save Atreyu from the Nothing", "scare the local bullies into a dumpster"],
+'Bastian')
+
+const smaug = new EvilDragon('Smaug',
+'black',
+["take over your mountain kingdom", "steal all your dwarven gold",  "burn down your floating village"],
+'Dwarf King')
+
+
+const allDragons = Dragons.getDragons(falkor, smaug);
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
@@ -7,6 +21,7 @@
 try {
   module.exports.falkor = falkor
   module.exports.smaug = smaug
+  module.exports.allDragons = allDragons
 } catch {
   module.exports = null;
 }
