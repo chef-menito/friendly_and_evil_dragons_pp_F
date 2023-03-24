045e81b41790513f2130478c3614e72d36cf772e
Manuel <mansal120590@gmail.com>
2023-03-23 20:24:38 -0700

    Phase 1 completed

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,18 @@
 // Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire () {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons (...instances) {
+    return instances.map(dragon => dragon.name);
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
